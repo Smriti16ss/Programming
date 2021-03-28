562ea2cbcd649abbc7d5ed68f4ae463249ca7678
Smriti16ss <81315472+Smriti16ss@users.noreply.github.com>
2021-03-28 15:22:57 +0530

    Create first_program.cpp

---
+++ Introduction to C++/Getting Started/first_program.cpp
@@ -0,0 +1,18 @@
+Problem: print "Hello world!"
+ ************************************************************************** 
+ Program:
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
+
+***************************************************************************
+Output:
+Hello world!
+
